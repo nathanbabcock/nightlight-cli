27cb156881cc86ffd703f3d2379af28e3c7bf974
nbabcock <nathan.r.babcock@gmail.com>
2023-07-30 16:10:04 -0500

    feat: add node.js cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 
-console.log('Hello World!')
+import { log } from 'node:console'
+import { argv } from 'node:process'
+import { NightLight } from './nightlight.js'
+
+async function main() {
+  if (argv.includes('help')) return printHelp()
+  if (argv.includes('version')) return log('1.0')
+  const nightlight = new NightLight()
+  if (argv.includes('on')) return nightlight.enable()
+  if (argv.includes('off')) return nightlight.disable()
+  if (argv.includes('toggle')) return nightlight.toggle()
+  if (argv.includes('status'))
+    return (await nightlight.enabled()) ? log('on') : log('off')
+  return printHelp()
+}
+
+function printHelp(): void {
+  console.log('Usage: nightlight [subcommand]')
+  console.log('nightlight help')
+  console.log('nightlight version')
+  console.log('nightlight on')
+  console.log('nightlight off')
+  console.log('nightlight toggle')
+  console.log('nightlight status')
+}
+
+main()
